116bcbf4e9fe3578d7e34b06f5ca64ce80cbfd6d
JHay0112 <jordanhay0112@gmail.com>
2023-02-02 14:10:24 +1300

    tweak loader to support new editor setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,64 @@
 import {
-  Component, 
-  lazy, 
-  Suspense, 
-  createSignal
+    Component,
+    lazy,
+    Suspense,
+    createSignal,
+    createResource
 } from 'solid-js';
 
 import loadPython from './py/lcapy';
 
 /**
- * Lazy loading canvas,
- * this awaits lcapy loading before import the actual canvas
+ * Python shell with lcapy
  */
-const Canvas = lazy(async () => {
-  let py = await loadPython();
-  return import("./ui/canvas");
+const [py] = createResource(loadPython);
+
+/**
+ * Loading ellipses spinner
+ */
+const [ellipses, setEllipses] = createSignal(".");
+
+/**
+ * Lazy loading editor,
+ * this awaits lcapy loading before import the actual editor.
+ * It also handles the updating of the ellipsis spinner
+ */
+const Editor = lazy(async () => {
+    while (py.loading) {
+        // check every 0.2 s
+        await new Promise(r => setTimeout(r, 200));
+        // update ellipses spinner
+        if (ellipses().length >= 3) {
+            setEllipses(".");
+        } else {
+            setEllipses(ellipses().concat("."));
+        }
+    }
+    return import("./ui/Editor");
 });
 
 /**
  * Loading screen,
- * used as a fallback before canvas load
+ * used as a fallback before editor load
  */
 const LoadingScreen = () => {
-
-  // create a continually updating set of ellipse dots
-  const [ellipses, setEllipses] = createSignal(".");
-  setInterval(
-    () => {
-      if (ellipses().length >= 3) {
-        setEllipses(".");
-      } else {
-        setEllipses(ellipses().concat("."));
-      }
-    }, 
-    400 // 0.4s delay between dot updates
-  );
-
-  // TODO: get feedback from python loader to pass onto the user
-  return (
-    <>
-      <section class="h-screen w-screen flex flex-col justify-center text-center bg-primary">
-        <h1>lcadide</h1>
-        <p><br /></p>
-        <p>loading dependencies</p>
-        <p>{ellipses()}</p>
-      </section>
-    </>
-  );
+    // TODO: get feedback from python loader to pass onto the user
+    return (<>
+        <section class="h-screen w-screen flex flex-col justify-center text-center bg-primary">
+            <h1>lcadide</h1>
+            <p><br /></p>
+            <p>loading dependencies</p>
+            <p>{ellipses()}</p>
+        </section>
+    </>);
 }
 
 const App: Component = () => {
-  return(
-    <>
-      <Suspense fallback={<LoadingScreen />}>
-        <Canvas />
-      </Suspense>
-    </>
-  );
+    return (<>
+        <Suspense fallback={<LoadingScreen />}>
+            <Editor />
+        </Suspense>
+    </>);
 };
 
 export default App;
---
+++ src/ui/Editor.tsx
@@ -0,0 +1,15 @@
+/**
+ * Provides the editor interface
+ */
+
+/**
+ * Editor
+ * Provides an interface for users to select, place, and edit schematics
+ */
+export default function Editor() {
+    return (
+        <>
+            <p>Editor loaded.</p>
+        </>
+    );
+}
