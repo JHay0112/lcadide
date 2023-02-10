61b778d3d6c5696884a236eea3454e8326a2846c
JHay0112 <jordanhay0112@gmail.com>
2023-02-10 13:30:05 +1300

    make resistor more consistent

--- src/model/components/resistor.tsx
+++ src/model/components/resistor.tsx
@@ -20,13 +20,13 @@ export default class Resistor extends Component {
         return `
             M ${this.pixelWidth/2} ,  0
             l   0                  , ${(this.pixelHeight - 35)/2}
+            l  10                  ,  5
+            l -20                  ,  5
             l  20                  ,  5
-            l -40                  ,  5
-            l  40                  ,  5
-            l -40                  ,  5
-            l  40                  ,  5
-            l -40                  ,  5
+            l -20                  ,  5
             l  20                  ,  5
+            l -20                  ,  5
+            l  10                  ,  5
             L ${this.pixelWidth/2} , ${this.pixelHeight}
         `;
     }
