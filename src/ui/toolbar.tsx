dfbb9a5affc7047b36f25c2dfacc0854100abe69
JHay0112 <jordanhay0112@gmail.com>
2023-02-11 14:36:45 +1300

    generify toolbar tools

--- src/ui/toolbar.tsx
+++ src/ui/toolbar.tsx
@@ -2,7 +2,7 @@
  * Handles editor toolbar
  */
 
-import { splitProps } from "solid-js";
+import { children, splitProps } from "solid-js";
 
 import Sheet from "../model/sheet";
 
@@ -10,6 +10,26 @@ import Resistor from "../model/components/resistor";
 import Inductor from "../model/components/inductor";
 import Capacitor from "../model/components/capacitor";
 import VoltageSource from "../model/components/voltage_source";
+
+/**
+ * Icon/button for a tool
+ */
+function Tool(props) {
+
+    // get onclick function
+    const [local, _] = splitProps(props, ["onClick"]);
+    const onClick = local.onClick;
+    // get text
+    const c = children(() => props.children);
+
+    return (<>
+        <button class="
+            px-4 py-2 transition-all hover:bg-primary hover:text-primary mx-2
+        " onClick={onClick}>{
+            c()
+        }</button>
+    </>);
+}
 
 /**
  * Bottom toolbar for editor interface
@@ -22,18 +42,18 @@ export default function Toolbar(props) {
 
     return (<>
         <section class={`text-secondary bg-secondary absolute inset-x-0 bottom-0 text-center transition-all ${local.class}`}>
-            <button class="px-4 py-2 transition-all hover:bg-primary hover:text-primary mx-2" onClick={() => {
+            <Tool onClick={() => {
                 sheet.activeComponent = new Resistor(sheet);
-            }}>R</button>
-            <button class="px-4 py-2 transition-all hover:bg-primary hover:text-primary mx-2" onClick={() => {
+            }}>R</Tool>
+            <Tool onClick={() => {
                 sheet.activeComponent = new Inductor(sheet);
-            }}>L</button>
-            <button class="px-4 py-2 transition-all hover:bg-primary hover:text-primary mx-2" onClick={() => {
+            }}>L</Tool>
+            <Tool onClick={() => {
                 sheet.activeComponent = new Capacitor(sheet);
-            }}>C</button>
-            <button class="px-4 py-2 transition-all hover:bg-primary hover:text-primary mx-2" onClick={() => {
+            }}>C</Tool>
+            <Tool onClick={() => {
                 sheet.activeComponent = new VoltageSource(sheet);
-            }}>V</button>
+            }}>V</Tool>
         </section>
     </>);
 }
