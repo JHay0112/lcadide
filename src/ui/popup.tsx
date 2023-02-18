49a518903f17e98eeaf11d56b0aec233b85779ce
JHay0112 <jordanhay0112@gmail.com>
2023-02-18 18:32:25 +1300

    fix context menu not showing up

--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -16,13 +16,14 @@ export default function Popup(props) {
     // get children
     const c = children(() => props.children);
     // get title
-    const [local, _] = splitProps(props, ["title", "onExit", "class"]);
+    const [local, _] = splitProps(props, ["title", "onExit", "class", "ref"]);
     const title: string = local.title;
     const onExit: () => void = local.onExit;
     const cls: string = local.class;
+    const ref = local.ref;
 
     return (<>
-        <div class="absolute top-0 left-0 w-screen h-screen bg-opacity-70 bg-neutral-900 z-50 flex items-center justify-center">
+        <div ref={ref} class="absolute top-0 left-0 w-screen h-screen bg-opacity-70 bg-neutral-900 z-50 flex items-center justify-center">
             <div class="w-10/12 h-10/12 md:w-8/12 bg-primary relative bottom-10 p-4 rounded-md">
                 <header class="mb-3">
                     <h1 class="inline-block p-2">{title}</h1>
